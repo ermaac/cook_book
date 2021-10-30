a79610d585029b25d746e0ea579783d4f4fd48a9
Vlad Ermolovich <vlad.ermolovich@external.tablecheck.com>
2021-10-30 22:02:24 +0300

    Add Ingredient model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_30_185624) do
+ActiveRecord::Schema.define(version: 2021_10_30_190109) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "ingredients", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["name"], name: "index_ingredients_on_name"
+  end
 
   create_table "recipes", force: :cascade do |t|
     t.string "name"
