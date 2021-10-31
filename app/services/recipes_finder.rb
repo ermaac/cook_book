27e591e50620bbd2a12bdb09c25ba5c2e2d72300
Vlad Ermolovich <vlad.ermolovich@external.tablecheck.com>
2021-10-31 21:57:10 +0300

    RecipeFinder: ignore special characters

--- app/services/recipes_finder.rb
+++ app/services/recipes_finder.rb
@@ -1,5 +1,6 @@
 class RecipesFinder
   WITH_INGREDIENTS_QUERY = 'array_agg(ingredient_id) <@ array[?]::bigint[]'.freeze
+  BLACKLISTED_SYMBOLS = '[^\p{L}\s\-\']'.freeze
 
   attr_reader :products
 
@@ -10,17 +11,18 @@ class RecipesFinder
   def recipes
     return [] unless products.present? && products_regexp.present?
 
-    available_ingredients = Ingredient.where('ingredients.name ~* ?', products_regexp)
-    Recipe.joins(:steps).group(:id).having(WITH_INGREDIENTS_QUERY, available_ingredients.ids)
+    available_ingredients = Ingredient.where('ingredients.name ~ ?', products_regexp)
+    Recipe.joins(:steps).group('recipes.id').having(WITH_INGREDIENTS_QUERY, available_ingredients.ids)
   end
 
   private
 
   def products_regexp
-    @products_regexp = products.map(&method(:with_word_boundries)).join('|')
+    @products_regexp ||= products.map(&method(:with_word_boundries)).join('|')
   end
 
   def with_word_boundries(product)
-    "\\y#{product}\\y" # Postgres: \y	matches only at the beginning or end of a word
+    sanitized_product_name = product.gsub(/#{BLACKLISTED_SYMBOLS}/, '')
+    "\\y#{sanitized_product_name}\\y" # Postgres: \y	matches only at the beginning or end of a word
   end
 end
